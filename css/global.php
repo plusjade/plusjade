eeb9b784632736898c02d93af22730979a0ea240
plusjade <plusjade@gmail.com>
2009-04-13 21:24:24 -0700

    updated theme files to work with the new system updates

--- css/global.php
+++ css/global.php
@@ -4,23 +4,12 @@ header('Expires: ' . date('r',time() + 864000));
 header("Pragma: public");
 header("Cache-Control: public");
 
-/*
- * we need this because GET variables do not  
- * pass on live linode server. (no idea why)
- */
-$user	= $_GET['u'];
-$theme	= $_GET['t'];
-	
-if(! empty($_SERVER['PATH_INFO']) )
-{
-	$pieces	= explode('/', $_SERVER['PATH_INFO']);
-	$user	= $pieces['1'];
-	$theme	= $pieces['2'];		
-}
+$user			= $_GET['u'];
+$theme			= $_GET['t'];	
 $ROOT			= $_SERVER['DOCUMENT_ROOT'];
-$user_css		= "$ROOT/data/$user/themes/$theme/global/global.css";	
-$root_css		= "$ROOT/application/views/$theme/global/global.css";
-$image_path		= "/data/$user/themes/$theme/global/images";
+$user_css		= "$ROOT/data/$user/themes/$theme/global.css";	
+$root_css		= "$ROOT/application/views/$theme/global.css";
+$image_path		= "/data/$user/themes/$theme/images";
 $static_helpers	= "$ROOT/application/views/_global/static_helpers.css";
 
 ob_start();
@@ -29,8 +18,8 @@ ob_start();
 readfile($static_helpers);
 
 /*
- * load custom global root css if available
- * else load global root css from theme
+ * Load custom global root css if available
+ * else: fallback to theme folder
  */
 if ( file_exists($user_css) )
 {
@@ -49,4 +38,4 @@ echo str_replace('%PATH%', $image_path , $global);
 	
 die();
 
-/* end of css/global.css */	
+/* end of css/global.php */	
---
+++ css/test/database.log
@@ -0,0 +1,1 @@
+Could not connect: Access denied for user 'root'@'localhost' (using password: YES)
