ae11c0eacbff00cef62c65dbed5f0c5088252889
plusjade <plusjade@gmail.com>
2009-06-12 23:58:18 -0700

    yaml can now parse/update simplified key/value pair files. much cleaner logic

--- application/helpers/yaml.php
+++ application/helpers/yaml.php
@@ -7,6 +7,67 @@
  */
 class yaml_Core {
 
+/* simple key = value functions for site_config */
+	
+	/*
+	 * Add or edit a key/value pair from a yaml file.
+	 */
+	public static function edit_site_value($site_name, $filename, $key, $new_value)
+	{
+		$config_path		= DATAPATH . "$site_name/protected/$filename.yaml";
+		$config_array		= self::parse_basic($site_name, $filename);		
+		$config_array[$key]	= $new_value;
+		return self::build_yaml_file($config_array, $config_path);
+	}
+
+	/*
+		parse the yaml file and return the key/value array
+	*/
+	public static function parse_basic($site_name, $filename)
+	{
+		$config_path = DATAPATH . "$site_name/protected/$filename.yaml";
+		if( file_exists($config_path) )
+		{
+			$yaml_array		= array();
+			$pages_config	= file_get_contents($config_path);
+			$pages_config	= explode(',', $pages_config);
+			
+			foreach($pages_config as $entry)
+			{
+				$pieces = explode('=', $entry);
+				$key	= trim(@$pieces['0']);
+				$value	= trim(@$pieces['1']);		
+				$yaml_array[$key] = $value;
+			}
+			return $yaml_array;
+		}			
+		return FALSE;	
+	}
+
+	/* regenerate and save edited yaml file
+	 * from an edited key/value array of values
+	 * should only be used internally
+	 */
+	private static function build_yaml_file($config_array, $config_path)
+	{
+		$contents = '';
+		foreach($config_array as $key => $value)
+			if(! empty($key) )
+				$contents .= "$key = $value ,\n";
+
+		if(file_put_contents($config_path, $contents))
+			return TRUE;
+		
+		return FALSE;
+	}
+/* END: simple key = value functions for site_config */
+	
+	
+	
+	
+	
+	
+
 	/*
 		parse the yaml file and return the key/value array
 	*/
@@ -78,6 +139,7 @@ class yaml_Core {
 		
 		return true;
 	}
+
 	
 	public static function edit_value($site_name, $filename, $old_value, $new_value )
 	{
@@ -89,11 +151,9 @@ class yaml_Core {
 			$new_content =  str_replace("$old_value:", "$new_value:" , $config_contents);
 			file_put_contents($config_path, $new_content);
 		}
-		return true;
+		return TRUE;
 	}
 	
-	
-
 	
 	public static function delete_value($site_name, $filename, $key)
 	{
